456888fa0e4c1edc5c28e794d7f885710bbf569f
yunhes45 <yunhes45@gmail.com>
2021-11-13 23:45:29 +0900

    [#83] test2.0

--- ChatprojApplicationTests.java
+++ ChatprojApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.chatproj.chatproj;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
+@SpringBootTest
 class ChatprojApplicationTests {
 
 	@Test
