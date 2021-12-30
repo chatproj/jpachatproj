d1d4cac103bb5d50413ec25d29beffaa61ccf63f
yunhes45 <yunhes45@gmail.com>
2021-12-30 23:33:43 +0900

    [#122] feat : 파일 전송

--- src/main/java/com/example/chatproj/chatproj/controller/controller.java
+++ src/main/java/com/example/chatproj/chatproj/controller/controller.java
@@ -18,12 +18,15 @@ import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
